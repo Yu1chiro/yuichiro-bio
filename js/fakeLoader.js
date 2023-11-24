a2e14e035a4528fddb00da4db805b213103f25b9
yu1chiro <takumiharu25@gmail.com>
2023-11-25 01:16:24 +0800

    update index

--- js/fakeLoader.js
+++ js/fakeLoader.js
@@ -14,7 +14,7 @@
             bgColor: '#040B12', 
             spinner:'spinner1'
         }, options);
-        var spinner01 = '<div class="fl fl-spinner spinner1" style="display: flex; justify-content: center; align-items: center;"><img src="hutao1.png" alt="Logo" style="width: 30rem; height: 30rem; animation: zoomInOut 1s ease-in-out infinite;"></div>';
+        var spinner01 = '<div class="fl fl-spinner spinner1" style="display: flex; justify-content: center; align-items: center;"><img src="hutao1.png" alt="Logo" style="width: 20rem; height: 20rem; animation: zoomInOut 1s ease-in-out infinite;"></div>';
         var spinner02 = '<div class="fl fl-spinner spinner2"><div class="spinner-container container1"><div class="circle1"></div><div class="circle2"></div><div class="circle3"></div><div class="circle4"></div></div><div class="spinner-container container2"><div class="circle1"></div><div class="circle2"></div><div class="circle3"></div><div class="circle4"></div></div><div class="spinner-container container3"><div class="circle1"></div><div class="circle2"></div><div class="circle3"></div><div class="circle4"></div></div></div>';
         var spinner03 = '<div class="fl fl-spinner spinner3"><div class="dot1"></div><div class="dot2"></div></div>';
         var spinner04 = '<div class="fl fl-spinner spinner4"></div>'; 
