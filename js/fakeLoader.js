6ad662788a8019dc815192d4031052d0f08a8dc7
yu1chiro <takumiharu25@gmail.com>
2023-11-25 01:11:40 +0800

    update loader

--- js/fakeLoader.js
+++ js/fakeLoader.js
@@ -10,18 +10,18 @@
 
         var settings = $.extend({
             targetClass:'fakeLoader',
-            timeToHide:1200,               
-            bgColor: '#2ecc71', 
-            spinner:'spinner2'
+            timeToHide:2000,               
+            bgColor: '#040B12', 
+            spinner:'spinner1'
         }, options);
-
-        var spinner01 = '<div class="fl fl-spinner spinner1"><div class="double-bounce1"></div><div class="double-bounce2"></div></div>';
+        var spinner01 = '<div class="fl fl-spinner spinner1" style="display: flex; justify-content: center; align-items: center;"><img src="hutao1.png" alt="Logo" style="width: 30rem; height: 30rem; animation: zoomInOut 1s ease-in-out infinite;"></div>';
         var spinner02 = '<div class="fl fl-spinner spinner2"><div class="spinner-container container1"><div class="circle1"></div><div class="circle2"></div><div class="circle3"></div><div class="circle4"></div></div><div class="spinner-container container2"><div class="circle1"></div><div class="circle2"></div><div class="circle3"></div><div class="circle4"></div></div><div class="spinner-container container3"><div class="circle1"></div><div class="circle2"></div><div class="circle3"></div><div class="circle4"></div></div></div>';
         var spinner03 = '<div class="fl fl-spinner spinner3"><div class="dot1"></div><div class="dot2"></div></div>';
         var spinner04 = '<div class="fl fl-spinner spinner4"></div>'; 
         var spinner05 = '<div class="fl fl-spinner spinner5"><div class="cube1"></div><div class="cube2"></div></div>'; 
         var spinner06 = '<div class="fl fl-spinner spinner6"><div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div></div>'; 
         var spinner07 = '<div class="fl fl-spinner spinner7"><div class="circ1"></div><div class="circ2"></div><div class="circ3"></div><div class="circ4"></div></div>'; 
+
 
         var el = $('body').find('.' + settings.targetClass);
 
